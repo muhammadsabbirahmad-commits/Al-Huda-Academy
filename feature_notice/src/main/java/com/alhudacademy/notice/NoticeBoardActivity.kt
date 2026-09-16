ed2164814cd17e118dc273843f5e90ddaac173cb
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:44:52 +0600

    Create NoticeBoardActivity.kt

---
+++ feature_notice/src/main/java/com/alhudacademy/notice/NoticeBoardActivity.kt
@@ -0,0 +1,16 @@
+package com.alhudacademy.notice
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_notice
+ * File: NoticeBoardActivity.kt
+ * Description: Notice board screen for displaying school announcements and alerts.
+ */
+class NoticeBoardActivity {
+
+    // TODO: Initialize notice variables and RecyclerView here
+    
+    fun publishNotice(title: String, message: String, date: String) {
+        // TODO: Save notice to database and notify users
+    }
+}
