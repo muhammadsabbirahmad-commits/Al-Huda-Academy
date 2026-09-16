61c00bd2e800575529337f6e6a9ae92cf20ad7e7
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:42:11 +0600

    Create AttendanceActivity.kt

---
+++ feature_attendance/src/main/java/com/alhudacademy/attendance/AttendanceActivity.kt
@@ -0,0 +1,16 @@
+package com.alhudacademy.attendance
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_attendance
+ * File: AttendanceActivity.kt
+ * Description: Digital attendance tracking screen for students and teachers.
+ */
+class AttendanceActivity {
+
+    // TODO: Initialize attendance tracking logic and views here
+    
+    fun markAttendance(studentId: String, status: Boolean) {
+        // TODO: Save attendance status to database
+    }
+}
