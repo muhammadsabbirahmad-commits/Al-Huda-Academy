2d5a7e5c224fb0d177ecf82405616fe695a2d07c
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:47:58 +0600

    Create HomeworkActivity.kt

---
+++ feature_homework/src/main/java/com/alhudacademy/homework/HomeworkActivity.kt
@@ -0,0 +1,16 @@
+package com.alhudacademy.homework
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_homework
+ * File: HomeworkActivity.kt
+ * Description: Homework and assignment management screen for students and teachers.
+ */
+class HomeworkActivity {
+
+    // TODO: Initialize homework variables and submission list here
+    
+    fun assignHomework(classId: String, title: String, dueDate: String) {
+        // TODO: Save homework details to database and notify students
+    }
+}
