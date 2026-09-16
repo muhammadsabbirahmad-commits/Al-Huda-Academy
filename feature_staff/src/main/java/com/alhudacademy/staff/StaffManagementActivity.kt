0e21bf4e6fad7b8732a11d2c659e5a0ceef2cee2
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:54:57 +0600

    Create StaffManagementActivity.kt

---
+++ feature_staff/src/main/java/com/alhudacademy/staff/StaffManagementActivity.kt
@@ -0,0 +1,17 @@
+package com.alhudacademy.staff
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_staff
+ * File: StaffManagementActivity.kt
+ * Description: Staff and teacher management, and payroll tracking screen.
+ */
+class StaffManagementActivity {
+
+    // TODO: Initialize staff variables and payroll list here
+    
+    fun calculateSalary(staffId: String, workingDays: Int): Double {
+        // TODO: Calculate monthly salary and save to database
+        return 0.0
+    }
+}
