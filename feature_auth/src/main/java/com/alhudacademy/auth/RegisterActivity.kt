3de2b2b2ecc9115c65f610465dca4b0052323aef
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:40:28 +0600

    Create RegisterActivity.kt

---
+++ feature_auth/src/main/java/com/alhudacademy/auth/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.alhudacademy.auth
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_auth
+ * File: RegisterActivity.kt
+ * Description: Registration screen logic for Al-Huda-Academy app.
+ */
+class RegisterActivity {
+
+    // TODO: Initialize registration variables and UI views here
+    
+    fun performRegister(email: String, pass: String, confirmPass: String) {
+        // TODO: Implement user registration and validation logic
+    }
+}
