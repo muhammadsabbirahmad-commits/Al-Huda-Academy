a12bf12f557f433952bec7a85cb92c5163fa3ebc
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:39:26 +0600

    Create LoginActivity.kt

---
+++ feature_auth/src/main/java/com/alhudacademy/auth/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.alhudacademy.auth
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_auth
+ * File: LoginActivity.kt
+ * Description: Login screen logic for Al-Huda-Academy app.
+ */
+class LoginActivity {
+
+    // TODO: Initialize authentication variables and UI views here
+    
+    fun performLogin(email: String, pass: String) {
+        // TODO: Implement login verification logic
+    }
+}
