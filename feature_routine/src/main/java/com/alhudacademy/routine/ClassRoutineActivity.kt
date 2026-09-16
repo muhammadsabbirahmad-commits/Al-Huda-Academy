822464f1e1c78f5947895904ea556f36457706d0
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:45:51 +0600

    Create ClassRoutineActivity.kt

---
+++ feature_routine/src/main/java/com/alhudacademy/routine/ClassRoutineActivity.kt
@@ -0,0 +1,17 @@
+package com.alhudacademy.routine
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_routine
+ * File: ClassRoutineActivity.kt
+ * Description: Class routine and timetable management screen for students and teachers.
+ */
+class ClassRoutineActivity {
+
+    // TODO: Initialize routine variables and schedule views here
+    
+    fun getDailyRoutine(dayOfWeek: String): String {
+        // TODO: Fetch routine for the specific day from database
+        return "Routine loaded successfully"
+    }
+}
