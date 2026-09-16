0948fc0863e333d45ba43014f7c2fbc519ccd1db
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:53:23 +0600

    Create HostelActivity.kt

---
+++ feature_hostel/src/main/java/com/alhudacademy/hostel/HostelActivity.kt
@@ -0,0 +1,16 @@
+package com.alhudacademy.hostel
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_hostel
+ * File: HostelActivity.kt
+ * Description: Hostel room allocation and student accommodation management screen.
+ */
+class HostelActivity {
+
+    // TODO: Initialize hostel variables and room list here
+    
+    fun allocateRoom(studentId: String, roomNo: String) {
+        // TODO: Save room allocation details to database
+    }
+}
