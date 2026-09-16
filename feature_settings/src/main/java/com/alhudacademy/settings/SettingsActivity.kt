624c9136bcb7d9358248886b0ad0e78452b1d050
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:56:21 +0600

    Create SettingsActivity.kt

---
+++ feature_settings/src/main/java/com/alhudacademy/settings/SettingsActivity.kt
@@ -0,0 +1,16 @@
+package com.alhudacademy.settings
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_settings
+ * File: SettingsActivity.kt
+ * Description: App settings, profile configuration, and preferences management screen.
+ */
+class SettingsActivity {
+
+    // TODO: Initialize shared preferences and settings UI here
+    
+    fun updateTheme(isDarkMode: Boolean) {
+        // TODO: Save theme preference and apply changes
+    }
+}
