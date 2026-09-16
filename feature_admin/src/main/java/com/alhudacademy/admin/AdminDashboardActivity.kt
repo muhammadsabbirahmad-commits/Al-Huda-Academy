a4e492f93a1ee6f28639d266ed3befc818ee0b74
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:41:20 +0600

    Create AdminDashboardActivity.kt

---
+++ feature_admin/src/main/java/com/alhudacademy/admin/AdminDashboardActivity.kt
@@ -0,0 +1,16 @@
+package com.alhudacademy.admin
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_admin
+ * File: AdminDashboardActivity.kt
+ * Description: Admin Dashboard screen for managing school data, teachers, and students.
+ */
+class AdminDashboardActivity {
+
+    // TODO: Initialize admin dashboard components and statistics here
+    
+    fun loadDashboardData() {
+        // TODO: Fetch student and teacher counts from database
+    }
+}
