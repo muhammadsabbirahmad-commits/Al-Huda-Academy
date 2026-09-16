dc84ca5c8e14063cccefa2eb5e2cbc14e8acf3b9
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:43:19 +0600

    Create FeeCollectionActivity.kt

---
+++ feature_fees/src/main/java/com/alhudacademy/fees/FeeCollectionActivity.kt
@@ -0,0 +1,16 @@
+package com.alhudacademy.fees
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_fees
+ * File: FeeCollectionActivity.kt
+ * Description: Fee collection and payment tracking screen for students.
+ */
+class FeeCollectionActivity {
+
+    // TODO: Initialize fee variables and UI views here
+    
+    fun collectFee(studentId: String, amount: Double, paymentType: String) {
+        // TODO: Save transaction details to database
+    }
+}
