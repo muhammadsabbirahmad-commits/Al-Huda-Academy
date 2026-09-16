f7ada385c5c21f0e8514f324c0281bb771872dc9
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:57:38 +0600

    Create MainActivity.kt

---
+++ app/src/main/java/com/alhudacademy/app/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.alhudacademy.app
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: app (Main Host)
+ * File: MainActivity.kt
+ * Description: Main entry point and dashboard hub for Al-Huda-Academy app.
+ */
+class MainActivity {
+
+    // TODO: Initialize main navigation and module launchers here
+    
+    fun launchApp() {
+        // TODO: Start application and navigate to Login or Dashboard
+    }
+}
