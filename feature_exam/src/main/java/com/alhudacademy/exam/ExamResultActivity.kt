4d502a40418defe3a949e8fe61dba88fdc1a6f63
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:44:04 +0600

    Create ExamResultActivity.kt

---
+++ feature_exam/src/main/java/com/alhudacademy/exam/ExamResultActivity.kt
@@ -0,0 +1,17 @@
+package com.alhudacademy.exam
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_exam
+ * File: ExamResultActivity.kt
+ * Description: Exam results and grading management screen for students.
+ */
+class ExamResultActivity {
+
+    // TODO: Initialize exam and result variables here
+    
+    fun calculateGrade(marks: Double): String {
+        // TODO: Implement grading logic (A+, A, B, etc.)
+        return "A+"
+    }
+}
