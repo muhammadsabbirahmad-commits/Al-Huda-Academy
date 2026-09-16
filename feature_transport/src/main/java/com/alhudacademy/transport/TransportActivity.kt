be3f8e49fb6ea9a55f23de6bc5e98d39c3428541
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:50:46 +0600

    Create TransportActivity.kt

---
+++ feature_transport/src/main/java/com/alhudacademy/transport/TransportActivity.kt
@@ -0,0 +1,17 @@
+package com.alhudacademy.transport
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_transport
+ * File: TransportActivity.kt
+ * Description: School transport and bus tracking management screen.
+ */
+class TransportActivity {
+
+    // TODO: Initialize transport variables and route map here
+    
+    fun trackBus(busId: String): String {
+        // TODO: Fetch route details or GPS location of the school bus
+        return "Bus location fetched successfully"
+    }
+}
