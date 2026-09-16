41dc82ba2c0bac4f4d12fd965e4280fe47d9ce56
muhammadsabbirahmad-commits <muhammadsabbirahmad@gmail.com>
2026-09-16 10:48:45 +0600

    Create LibraryActivity.kt

---
+++ feature_library/src/main/java/com/alhudacademy/library/LibraryActivity.kt
@@ -0,0 +1,16 @@
+package com.alhudacademy.library
+
+/**
+ * Project: Al-Huda-Academy
+ * Module: feature_library
+ * File: LibraryActivity.kt
+ * Description: Library and book issue management screen for students.
+ */
+class LibraryActivity {
+
+    // TODO: Initialize library variables and book list here
+    
+    fun issueBook(bookId: String, studentId: String, issueDate: String) {
+        // TODO: Save book issue details to database
+    }
+}
